eaadeecbc20826f7798be8cccebdf8f40bfb4284
Lucas Vieira <newlucasfevi@gmail.com>
2016-03-17 17:47:41 -0500

    Add showBuyer and hideBuyer methods (not finished). Also changed some variables names.

--- app/components/million-image/million-image.component.js
+++ app/components/million-image/million-image.component.js
@@ -10,27 +10,40 @@
     function millionImageController($log) {
       const vm = this;
 
-      vm.width = [];
-      vm.height = [];
+      vm.rows = [];
+      vm.cols = [];
       vm.meals = [];
       vm.isBought = isBought;
+      vm.showBuyer = showBuyer;
+      vm.hideBuyer = hideBuyer;
 
       activate();
 
       function activate() {
         for (let i = 0; i < 250; i++) {
-          vm.width.push(i);
-          vm.height.push(i);
+          vm.rows.push(i);
+          vm.cols.push(i);
           vm.meals.push([]);
           for (let j = 0; j < 250; j++) {
             var rand = Math.random();
-            vm.meals[i].push({ bought: rand < 0.25 });
+            vm.meals[i].push({
+              bought: rand < 0.25,
+              buyer: 'John Doe'
+            });
           }
         }
       }
 
       function isBought(row, col) {
         return vm.meals[row][col].bought;
+      }
+
+      function showBuyer(row, col) {
+        console.log(vm.meals[row][col].buyer);
+      }
+
+      function hideBuyer(row, col) {
+        // console.log(vm.meals[row][col].buyer);
       }
     }
 
