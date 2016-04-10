c417f27c87e35a34da1dac4744280a353f080052
Matheus Sampaio <matheus1401@gmail.com>
2016-04-10 18:32:05 -0500

    fix: version and name

--- app/app.config.js
+++ app/app.config.js
@@ -13,13 +13,7 @@
     });
   }
 
-
   function fmscConfig($stateProvider, $urlRouterProvider) {
-    // Ionic uses AngularUI Router which uses the concept of states
-    // Learn more here: https://github.com/angular-ui/ui-router
-    // Set up the various states which the app can be in.
-    // Each state's controller can be found in controllers.js
-
     const appState = {
       url: '/',
       template: '<app></app>'
