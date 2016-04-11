a34072fa886fcd57202560d119c07eb42694cdcc
Matheus Sampaio <matheus1401@gmail.com>
2016-04-11 18:23:07 -0500

    feat: add firebase debug url

--- app/components/constants/fmsc.constants.js
+++ app/components/constants/fmsc.constants.js
@@ -2,6 +2,7 @@
 
   angular
     .module('fmsc')
+    .constant('DEBUGFirebaseURL', 'https://fmsc-donation-debug.firebaseio.com/')
     .constant('FirebaseURL', 'https://fmsc-donation1.firebaseio.com/');
 
 })();
