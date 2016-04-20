feab1563f7884313a8076b84f6bf8016456f7550
Matheus Sampaio <matheus1401@gmail.com>
2016-04-20 13:25:02 -0500

    chore: add paypal-ipn

--- server/models/paypal.js
+++ server/models/paypal.js
@@ -1,35 +1,14 @@
-const request = require('request');
+const ipn = require('paypal-ipn');
 const express = require('express');
 const paypal = express();
 
-// receive IPN
 paypal.post('/', (req, res) => {
-  console.log(req, res);
   res.status(200).send();
-
-  replyIPN(req, res).then(() => {
-    handlePayment(req);
-  });
+  ipn.verify(req.body, { allow_sandbox: true }, handlePayment);
 });
 
-
-function replyIPN(req) {
-  console.log('reply ipn');
-  console.log(req.get('host'), req.get('body'));
-
-  return new Promise(resolve => {
-    request.post({ url: req.get('host'), formData: req.get('body') }, (error, response, body) => {
-      console.log(error, response, body);
-      resolve();
-    });
-  });
-}
-
-/**
- * TODO
- */
-function handlePayment(req) {
-  console.log('handlePayment');
+function handlePayment(...argv) {
+  console.log(argv);
 }
 
 module.exports = paypal;
